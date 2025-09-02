307d584cc93363480aacbb470cd5f2a54e7e36e7
yuyankan <89712442+yuyankan@users.noreply.github.com>
2025-09-02 13:17:52 +0800

    Create 01_mount.sh

---
+++ container_server_mount_server/01_mount.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo mount -t cifs //147.121.160.30/data /home/carenk/my_data/01_projects/04_isra/02_fastapi/mnt/images -o username=0185972,password=hihi202612345678,domain=AP,vers=3.0
